a95d2834a037b4738f93e45998fae1dbf52f99ef
Chongtian Gao <chongtiangao@gmail.com>
2020-07-26 11:07:06 -0500

    Create prime_decomposition.py

---
+++ prime_decomposition.py
@@ -0,0 +1,18 @@
+# Prime Decomposition
+import math
+from random import *
+x = randint(10000000, 1000000000)
+print(x)
+count = 0
+i = 2
+primes = []
+while(i <= math.sqrt(x)):
+    if(x % i == 0):
+        primes.append(i)
+        x /= i
+    else:
+        i += 1
+    count += 1
+primes.append(int(x))
+print(primes)
+print(count)
