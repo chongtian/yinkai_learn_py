aee59fe5a38fe30f3b04ff0afd1e590db4117aef
Tony Gao <txg0513@p2es.com>
2020-08-27 09:16:48 -0500

    check-in

--- quantopian.com/notebooks/daily_check_ma.py
+++ quantopian.com/notebooks/daily_check_ma.py
@@ -5,9 +5,7 @@ It compares moving averages with the price and gives recommendation.
 
 # Import pipeline built-ins.
 from quantopian.pipeline import Pipeline
-from quantopian.pipeline.factors import SimpleMovingAverage
-from quantopian.pipeline.factors import EWMA, ExponentialWeightedMovingAverage
-from quantopian.pipeline.factors import PercentChange
+from quantopian.pipeline.factors import SimpleMovingAverage, EWMA, ExponentialWeightedMovingAverage, PercentChange, RSI
 from quantopian.pipeline.data import EquityPricing
 from quantopian.pipeline.filters import StaticAssets
 from quantopian.research import run_pipeline
@@ -19,23 +17,23 @@ slow_ma_periods = 20
 fast_ma_periods = 10
 assets = [
     symbols('VGT'),
-    symbols('STAG'),
-    symbols('TAN'),
+    symbols('ESPO'),
     symbols('IBUY'),
-    symbols('ADMA'),
+    symbols('TAN'),
+    symbols('WCLD'),
     symbols('ARKF'),
     symbols('ARKG'),
     symbols('ARKW'),
-    symbols('CHNG'),
-    symbols('ESPO'),
+    symbols('STAG'),
     symbols('IRT'),
+    symbols('CHNG'),
 ]
-
 
 # Define factors.
 fast_ma = SimpleMovingAverage(inputs=[EquityPricing.close], window_length=fast_ma_periods)
 slow_ma = SimpleMovingAverage(inputs=[EquityPricing.close], window_length=slow_ma_periods)
-ema = EWMA.from_span(inputs=[EquityPricing.close], window_length=slow_ma_periods*2+20, span=slow_ma_periods,)
+# ema = EWMA.from_span(inputs=[EquityPricing.close], window_length=slow_ma_periods*2+20, span=slow_ma_periods,)
+rsi = RSI(inputs=[EquityPricing.close], window_length=5)
 price = EquityPricing.close.latest
 sell = price < fast_ma and price < slow_ma
 buy = price > fast_ma and price > slow_ma
@@ -49,7 +47,8 @@ pipe = Pipeline(
         'yersterday_price': price,
         'ma_fast': fast_ma,
         'ma_slow': slow_ma,
-        'ema': ema,
+        #'ema': ema,
+        'rsi': rsi,
         'signal_sell': sell,
         'signal_buy': buy
     },
@@ -62,9 +61,23 @@ start = (date.today() - timedelta(days=100)).strftime('%Y-%m-%d')
 my_pipeline_result = run_pipeline(pipe, start, end)
 
 # show results of last two days
-retro_days = 1
-indecies = []
-for i in range(len(assets)):
-    indecies.append(-1*(i+1))
-    indecies.append(-1*(i+1) - len(assets)*retro_days)
-my_pipeline_result.iloc[indecies]
+#retro_days = 1
+#indecies = []
+#for i in range(len(assets)):
+#    indecies.append(-1*(i+1))
+#    indecies.append(-1*(i+1) - len(assets)*retro_days)
+#my_pipeline_result.iloc[indecies]
+
+# display summarized result
+# these dates are used as the first index
+current_date = (date.today() - timedelta(days=0)).strftime('%Y-%m-%d 00:00:00+00:00')
+previous_date = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d 00:00:00+00:00')
+for asset in assets:
+    current = my_pipeline_result.loc[(current_date,asset)]
+    previous = my_pipeline_result.loc[(previous_date,asset)]
+    signal = 'None'
+    if current.signal_buy and previous.signal_buy:
+        signal = 'Buy'
+    if current.signal_sell and previous.signal_sell:
+        signal = 'Sell'
+    print('{0} {1} RSI={2:.2f}'.format(str(asset).ljust(22), signal.ljust(8), current.rsi))
